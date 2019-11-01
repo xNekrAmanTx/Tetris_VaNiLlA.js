f6e81152730565b41a914cdadd8c58a2b1a25bd3
Sarkis Markosyan <sqe121@gmail.com>
2019-11-01 20:21:06 +0400

    second commit/first working test with basic functionality and correct preventDef

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let width = 10,
-    height = 20;
+    height = 20,
+    figWidth = 4,
+    figHeight = 4;
 
-width += 4;
-height += 2;
+width += figWidth;
+height += figHeight / 2;
 
-let derIjnumEm = false,
+let isFlying = false,
     inProgress = false,
     currentFig,
     ms,
@@ -13,10 +15,10 @@ let derIjnumEm = false,
 
 for (let i = 0; i < height; i++) {
     let tr = document.createElement('tr');
-    if (i < 2) tr.hidden = true;
+    if (i < figHeight / 2) tr.hidden = true;
     for (let j = 0; j < width; j++) {
         let td = document.createElement('td');
-        if (j < 2 || j > width - 3) td.hidden = true;
+        if (j < figWidth / 2 || j > width - 1 - figWidth / 2) td.hidden = true;
         td.innerHTML = `${i} ${j}`;
         tr.append(td)
     }
@@ -37,23 +39,28 @@ const randomInt0To = len => ~~(Math.random() * len)
 
 const chooseRandomFig = arr => { currentFig = arr[randomInt0To(arr.length)] }
 
+// const chooseRandomRotation = () => {
+//     for (let k = 0; k < randomInt0To(4); k++) rotateFigure();
+// }
+
 function rotateFigure() {
     let rotatedFig = [];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < figWidth; i++) {
         rotatedFig.push([])
-        for (let j = 0; j < 4; j++) {
-            rotatedFig[i][j] = currentFig[3 - j][i]
+        for (let j = 0; j < figHeight; j++) {
+            rotatedFig[i][j] = currentFig[figWidth - 1 - j][i]
         }
     }
-    // if (isPossible(rotatedFig, x, y)) {
-        popFigure(x, y);
+    popFigure(x, y);
+    if (!isPossible(rotatedFig, x, y)) pushFigure(x, y);
+    else {
         currentFig = rotatedFig;
         pushFigure(x, y)
-    // }
+    }
 }
 
 function isPossible(fig, x, y) {
-    return !fig.some((row, i) => row.some((val, j) => val && (dataMtx[y + i][x + j] || j < 2 || j > width - 3 || y >= height)))
+    return !fig.some((row, i) => row.some((val, j) => val && (dataMtx[Math.min(height-1, y + i)][x + j] || x + j < 2 || x + j > width - 3 || y + i >= height)))
 }
 
 function popFigure(x, y) {
@@ -65,24 +72,23 @@ function pushFigure(x, y) {
     render();
 }
 
-// insertFigure(figures[0])
 
 function fall() {
-    // if (!isPossible(currentFig, x, y + 1)) return;
-    popFigure(x, y);
-    pushFigure(x, ++y);
+    popFigure(x, y)
+    if (!isPossible(currentFig, x, y + 1)) pushFigure(x, y);
+    else pushFigure(x, ++y);
 }
 
 function pushLeft() {
-    // if (!isPossible(currentFig, x - 1, y)) return;
     popFigure(x, y);
-    pushFigure(--x, y);
+    if (!isPossible(currentFig, x - 1, y)) pushFigure(x, y);
+    else pushFigure(--x, y);
 }
 
 function pushRight() {
-    // if (!isPossible(currentFig, x + 1, y)) return;
     popFigure(x, y);
-    pushFigure(++x, y);
+    if (!isPossible(currentFig, x + 1, y)) pushFigure(x, y);
+    else pushFigure(++x, y);
 }
 
 const rowIsFilled = ind => dataMtx[ind].slice(2, -2).every(el => el)
@@ -95,7 +101,7 @@ function cleanRow(i) {
     const tr = document.createElement('tr');
     for (let j = 0; j < width; j++) {
         let td = document.createElement('td');
-        if (j < 2 || j > width - 3) td.hidden = true;
+        if (j < figWidth / 2 || j > width - 1 - figWidth / 2) td.hidden = true;
         tr.append(td);
     }
     table.rows[2].before(tr);
@@ -103,11 +109,15 @@ function cleanRow(i) {
 
 document.body.addEventListener('keydown', function handler(e) {
     if (!currentFig) return;
-    e.preventDefault();
+
+    if (e.keyCode > 36 && e.keyCode < 41) e.preventDefault(); // if(event.key.slice(,5) === 'Arrow')
+
     if (e.key === 'ArrowLeft') pushLeft();
     if (e.key === 'ArrowRight') pushRight();
     if (e.key === 'ArrowUp') rotateFigure();
-    if (e.key === 'ArrowDown') fall();
+    if (e.key === 'ArrowDown') {
+        fall()
+    };
 })
 
 while (true) {
@@ -116,12 +126,8 @@ while (true) {
     chooseRandomFig(figures);
     pushFigure(x, y);
     break;
-    // fall();
-    while (derIjnumEm) {
-        for (let i = 2; i < height; i++) if (rowIsFilled(i)) cleanRow(i)
-    }
+    // let timer = setInterval(()=>{
+    //     for (let i = 2; i < height; i++) if (rowIsFilled(i)) cleanRow(i)
+    // }
 }
 
-// function isPossible(fig, x, y) {
-//     return !fig.some((row, i) => row.some((val, j) => val && (dataMtx[Math.min(height-1, y + i)][x + j < 0 ? 0 : x + j > width - 1 ? width-1 : x + j ] || x + j < 2 || x + j > width - 3 || y + i >= height)))
-// }
